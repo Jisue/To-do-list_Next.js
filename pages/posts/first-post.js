d1631a50a3315c9eb036ec436ae86f4504a6e1f5
Jisue <49144667+Jisue@users.noreply.github.com>
2021-02-23 11:35:28 +0900

    next css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
-    <Head>
+    <Layout>
+      <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
@@ -13,6 +14,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
